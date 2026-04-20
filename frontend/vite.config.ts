4d6ed652cfe1364e2c752460c2724518ba51bf25
finanfixadm-oss <finanfix.adm@gmail.com>
2026-04-20 12:04:06 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true, // Esto permitirá que crm.finanfix.cl entre sin problemas
+    host: true,
     port: 5173
   }
-});
+})
