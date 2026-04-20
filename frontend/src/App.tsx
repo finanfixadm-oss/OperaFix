0e5029dfb521f9a061b4df844d80e0c7676b12b1
finanfixadm-oss <finanfix.adm@gmail.com>
2026-04-20 15:32:04 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
+import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import CompaniesPage from "./pages/CompaniesPage";
 import CollaboratorsPage from "./pages/CollaboratorsPage";
@@ -12,16 +14,25 @@ import TpRecordsPage from "./pages/TpRecordsPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Layout />
+          </PrivateRoute>
+        }
+      >
         <Route index element={<DashboardPage />} />
-        <Route path="/empresas" element={<CompaniesPage />} />
-        <Route path="/colaboradores" element={<CollaboratorsPage />} />
-        <Route path="/documentos" element={<DocumentsPage />} />
-        <Route path="/grupos-lm" element={<LmGroupsPage />} />
-        <Route path="/registros-empresas" element={<LmRecordsPage />} />
-        <Route path="/grupos-tp" element={<TpGroupsPage />} />
-        <Route path="/gestiones-tp" element={<TpRecordsPage />} />
+        <Route path="empresas" element={<CompaniesPage />} />
+        <Route path="colaboradores" element={<CollaboratorsPage />} />
+        <Route path="documentos" element={<DocumentsPage />} />
+        <Route path="grupos-lm" element={<LmGroupsPage />} />
+        <Route path="registros-empresas" element={<LmRecordsPage />} />
+        <Route path="grupos-tp" element={<TpGroupsPage />} />
+        <Route path="gestiones-tp" element={<TpRecordsPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
