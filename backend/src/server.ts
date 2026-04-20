5317681e2f06bdf05b5b05ddd9202e9736130804
finanfixadm-oss <finanfix.adm@gmail.com>
2026-04-20 16:46:16 -0400

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import { app } from "./app.js";
+
+app.get("/", (_req, res) => {
+  res.json({ status: "OK DESDE SERVER" });
+});
 import { env } from "./config/env.js";
 import { ensureUploadDir } from "./utils/storage.js";
+
+
+
 
 ensureUploadDir();
 
