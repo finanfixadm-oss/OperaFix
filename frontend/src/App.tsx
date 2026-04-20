51162a9f7ff803ca623b0bb9d522b3fe2ebc764f
finanfixadm-oss <finanfix.adm@gmail.com>
2026-04-20 19:49:46 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import DashboardPage from "./pages/DashboardPage";
+import CompaniesPage from "./pages/CompaniesPage";
+import CollaboratorsPage from "./pages/CollaboratorsPage";
+import DocumentsPage from "./pages/DocumentsPage";
+import LmGroupsPage from "./pages/LmGroupsPage";
 import LmRecordsPage from "./pages/LmRecordsPage";
+import TpGroupsPage from "./pages/TpGroupsPage";
+import TpRecordsPage from "./pages/TpRecordsPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<LmRecordsPage />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<DashboardPage />} />
+        <Route path="/empresas" element={<CompaniesPage />} />
+        <Route path="/colaboradores" element={<CollaboratorsPage />} />
+        <Route path="/documentos" element={<DocumentsPage />} />
+        <Route path="/grupos-lm" element={<LmGroupsPage />} />
+        <Route path="/registros-empresas" element={<LmRecordsPage />} />
+        <Route path="/grupos-tp" element={<TpGroupsPage />} />
+        <Route path="/gestiones-tp" element={<TpRecordsPage />} />
+      </Route>
+    </Routes>
   );
 }
