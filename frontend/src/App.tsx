c3196c0cd764f51a1e1050682836c25b39dc9b73
finanfixadm-oss <finanfix.adm@gmail.com>
2026-04-20 15:58:28 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
 import CompaniesPage from "./pages/CompaniesPage";
 import CollaboratorsPage from "./pages/CollaboratorsPage";
 import DocumentsPage from "./pages/DocumentsPage";
@@ -8,18 +8,12 @@ import LmGroupsPage from "./pages/LmGroupsPage";
 import LmRecordsPage from "./pages/LmRecordsPage";
 import TpGroupsPage from "./pages/TpGroupsPage";
 import TpRecordsPage from "./pages/TpRecordsPage";
-import ReportsPage from "./pages/ReportsPage";
-import AnalysisPage from "./pages/AnalysisPage";
-import RequestsPage from "./pages/RequestsPage";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/informes" element={<ReportsPage />} />
-        <Route path="/analisis" element={<AnalysisPage />} />
-        <Route path="/solicitudes" element={<RequestsPage />} />
+        <Route index element={<DashboardPage />} />
         <Route path="/empresas" element={<CompaniesPage />} />
         <Route path="/colaboradores" element={<CollaboratorsPage />} />
         <Route path="/documentos" element={<DocumentsPage />} />
